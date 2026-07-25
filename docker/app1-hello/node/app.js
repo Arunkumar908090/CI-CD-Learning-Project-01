7d8b4ba4e859f45b6f5c55be92c897ac63126638
Arunkumar908090 <popp77038@gmail.com>
2026-07-25 21:52:04 +0530

    Fix typos in environment variable names

--- docker/app1-hello/node/app.js
+++ docker/app1-hello/node/app.js
@@ -1,15 +1,15 @@
-const express = requir("express");
+const express = require("express");
 
 const app = express();
 
-const ENV = process.env.ENV_VALU || "No env set";
+const ENV = process.env.ENV_VALUE || "No env set";
 const HOSTNAME = process.env.HOSTNAME || require("os").hostname();
 
 app.get("/", (req, res) => {
     res.json({
         message: "Hello from Simple App (Node)",
         env: ENV,
-        container: HOSTNAM
+        container: HOSTNAME
     });
 });
 
